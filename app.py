c0077ec19c62c5898ea3c63027701c08037a4903
afonsosr2 <afonsosr2@gmail.com>
2024-01-24 14:40:39 -0300

    atualizando app.py

--- app.py
+++ app.py
@@ -7,46 +7,57 @@ from utils import atualizando_dados_ipea
 
 dados = pd.read_csv(atualizando_dados_ipea())
 
-# Criando a página do Streamlit
+###### Criando a página do Streamlit ######
 
 # Página dos modelos de previsão do petróleo Brent
-st.write("# \U0001f6e2\uFE0F Análise de preços do Petróleo Brent")
+def main():
+    st.write("# \U0001f6e2\uFE0F Análise de preços do Petróleo Brent")
 
-st.write("### Período da previsão")
-semanas = st.slider('Semanas de previsão:', 1, 52)
-periodo = semanas * 7
+    st.write("### Período da previsão")
+    semanas = st.slider('Semanas de previsão:', 1, 52)
+    periodo = semanas * 7
 
-st.write("### Modelo de Machine Learning")
-input_modelo = st.selectbox("Selecione o modelo que deseja utilizar:", ["Modelo_1", "Prophet"])
+    st.write("### Modelo de Machine Learning")
+    input_modelo = st.selectbox("Selecione o modelo que deseja utilizar:", ["Modelo_1", "Prophet"])
 
-st.subheader('Últimos 5 dias')
-st.write(dados.tail())
+    st.subheader('Últimos 5 dias')
+    st.write(dados.tail())
 
-# Gráfico dos dados atuais
-def plot_raw_data():
-	fig = go.Figure()
-	fig.add_trace(go.Scatter(x=dados['Data'], y=dados['Preço - petróleo bruto - Brent (FOB)'], name="Preço do Petróleo Brent"))
-	fig.layout.update(title_text='Preço do Petróleo Brent (FOB)', xaxis_rangeslider_visible=True)
-	st.plotly_chart(fig)
-	
-plot_raw_data()
+    # Gráfico dos dados atuais
+    def plot_raw_data():
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=dados['Data'], y=dados['Preço - petróleo bruto - Brent (FOB)'], name="Preço do Petróleo Brent"))
+        fig.layout.update(title_text='Preço do Petróleo Brent (FOB)', xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
+        
+    plot_raw_data()
 
-if(input_modelo == "Prophet"):
-    # Previsão com Prophet
+    if(input_modelo == "Prophet"):
+        prophet_prediction(periodo)
+    if(input_modelo == "Modelo_1"):
+        modelo_1_prediction(periodo)
+
+# Previsão com Prophet
+@st.cache_data
+def prophet_prediction(periodo_previsao):
     df_train = dados[['Data','Preço - petróleo bruto - Brent (FOB)']]
     df_train = df_train.rename(columns={"Data": "ds", "Preço - petróleo bruto - Brent (FOB)": "y"})
 
     m = Prophet()
     m.fit(df_train)
-    future = m.make_future_dataframe(periods=periodo)
+    future = m.make_future_dataframe(periods=periodo_previsao)
     forecast = m.predict(future)
 
     # Show and plot forecast
     st.subheader('Previsão')
     st.write(forecast.tail())
         
-    st.write(f'### Gráfico de previsão em {periodo} dias')
+    st.write(f'### Gráfico de previsão em {periodo_previsao} dias')
     plot_prev_prophet = plot_plotly(m, forecast)
     st.plotly_chart(plot_prev_prophet)
-if(input_modelo == "Modelo_1"):
+
+def modelo_1_prediction(periodo_previsao):
     st.text('Em construção...')
+
+if __name__ == '__main__':
+    main()
